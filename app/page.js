12a0f8d23f55973106859f02d7e0874b5d773ed3
varun sammal <varunsammal90@gmail.com>
2026-05-22 20:09:39 +0530

    home page updated

--- app/page.js
+++ app/page.js
@@ -1,19 +1,5 @@
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <Card className="w-64 m-8">
-      <CardHeader>
-        <CardTitle>Hello</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p>Shadcn is working!</p>
-      </CardContent>
-      <Button>Click me</Button>
-    </Card>
-
-
-  )
+  redirect("/dashboard");
 }
